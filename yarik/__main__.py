c4eb4112203ae97df3dd4cd18258aef37669d040
Alexey Gerasev <alexey.gerasev@gmail.com>
2024-10-06 16:48:53 +0700

    Mouse: Support different items

--- yarik/__main__.py
+++ yarik/__main__.py
@@ -1,6 +1,17 @@
+from dataclasses import dataclass
 from random import Random
 import pygame as pg
 from . import utils
+
+
+class Exit(Exception):
+    pass
+
+
+@dataclass
+class Item:
+    pos: pg.Vector2
+    image: pg.Surface
 
 
 def main():
@@ -9,39 +20,58 @@ def main():
     pg.init()
     screen = pg.display.set_mode((viewport.x, viewport.y))
 
-    mouse = pg.transform.scale_by(pg.image.load("assets/mouse.png").convert_alpha(), 2)
-    cheese = pg.transform.scale_by(
-        pg.image.load("assets/cheese.png").convert_alpha(), 2
+    player_image = pg.transform.scale_by(
+        pg.image.load("assets/mouse.png").convert_alpha(), 2
+    )
+    player_size = pg.Vector2(player_image.get_size())
+
+    item_variants = [
+        ("assets/cheese.png", 0.8),
+        ("assets/apple.png", 0.2),
+    ]
+    item_images = [
+        pg.transform.scale_by(pg.image.load(path).convert_alpha(), 2)
+        for path, _ in item_variants
+    ]
+    item_probabilities = [p for _, p in item_variants]
+    item_size = pg.Vector2(item_images[0].get_size())
+    assert all(
+        [item_size == pg.Vector2(item_image.get_size()) for item_image in item_images]
     )
 
-    running = True
+    font = pg.font.Font(None, 80)
+
+    rng = Random()
 
     def play_game():
         clock = pg.time.Clock()
         dt = 0.0
         timeout = 1.0
+        counter = 0
 
         player_pos = viewport / 2
-        player_size = pg.Vector2(mouse.get_size())
         speed = 300.0
 
-        rng = Random(0xDEADBEEF)
-        item_size = pg.Vector2(cheese.get_size())
         items = [
-            utils.random_uniform(rng, item_size / 2, viewport - item_size / 2)
+            Item(
+                pos=utils.random_uniform(rng, item_size / 2, viewport - item_size / 2),
+                image=rng.choices(item_images, item_probabilities)[0],
+            )
             for i in range(16)
         ]
 
         while True:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
-                    running = False
-                    break
+                    raise Exit()
 
             screen.fill("black")
-            for item_pos in items:
-                screen.blit(cheese, item_pos - item_size / 2)
-            screen.blit(mouse, player_pos - player_size / 2)
+            for item in items:
+                screen.blit(item.image, item.pos - item_size / 2)
+            screen.blit(player_image, player_pos - player_size / 2)
+
+            text_surface = font.render(f"{counter}", True, "white")
+            screen.blit(text_surface, (10, 10))
 
             keys = pg.key.get_pressed()
             if keys[pg.K_UP] or keys[pg.K_w]:
@@ -57,13 +87,14 @@ def main():
                 player_pos, player_size / 2, viewport - player_size / 2
             )
 
-            items = [
-                item_pos
-                for item_pos in items
-                if not utils.overlap(
-                    player_pos, item_pos, player_size / 3, item_size / 3
-                )
-            ]
+            new_items = []
+            for item in items:
+                if utils.overlap(player_pos, item.pos, player_size / 3, item_size / 3):
+                    counter += 1
+                else:
+                    new_items.append(item)
+            items = new_items
+
             if len(items) == 0:
                 if timeout > 0.0:
                     timeout -= dt
@@ -74,8 +105,13 @@ def main():
 
             dt = clock.tick(60) / 1000
 
-    while running:
-        play_game()
+        return True
+
+    while True:
+        try:
+            play_game()
+        except Exit:
+            break
     pg.quit()
 
 
