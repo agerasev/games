427d5ae64f15de87b12b2aa958bb05ee3367f04b
Alexey Gerasev <alexey.gerasev@gmail.com>
2024-10-17 17:31:34 +0700

    Add camera control and mouse grabbing

--- src/games/drive.rs
+++ src/games/drive.rs
@@ -1,15 +1,25 @@
 use crate::model::load_model;
 use anyhow::Error;
+use defer::defer;
 use macroquad::{
-    camera::{set_camera, set_default_camera, Camera2D},
+    camera::{set_camera, set_default_camera, Camera3D},
     color,
-    input::{is_key_down, KeyCode},
-    math::{Rect, Vec2},
+    input::{
+        is_key_down, is_key_pressed, is_mouse_button_down, mouse_delta_position, mouse_wheel,
+        set_cursor_grab, show_mouse, KeyCode, MouseButton,
+    },
+    math::{EulerRot, Quat, Rect, Vec2, Vec3},
+    miniquad::window::screen_size,
     models::{draw_mesh, Mesh},
     texture::{load_texture, set_default_filter_mode, FilterMode},
     window::{clear_background, next_frame},
 };
-use std::{future::Future, pin::Pin};
+use std::{f32::consts::PI, future::Future, pin::Pin};
+
+fn grab(state: bool) {
+    set_cursor_grab(state);
+    show_mouse(!state);
+}
 
 pub async fn main() -> Result<(), Error> {
     set_default_filter_mode(FilterMode::Linear);
@@ -18,10 +28,35 @@ pub async fn main() -> Result<(), Error> {
         ..load_model("l200.obj").await?
     };
 
+    let mut grabbed = true;
+    grab(grabbed);
+    defer!(grab(false));
+
+    let mouse_sens = Vec2::new(5e-4, 5e-4);
+    let wheel_sens: f32 = 0.2;
+    let (mut r, mut phi, mut theta) = (10.0, 0.0, 0.0);
     while !is_key_down(KeyCode::Escape) {
         {
-            let camera = Camera2D {
-                zoom: Vec2::splat(0.2),
+            if is_key_pressed(KeyCode::Tab) {
+                grabbed ^= true;
+                grab(grabbed);
+            }
+            let scroll = mouse_wheel().1;
+            if scroll != 0.0 {
+                r *= (1.0 + wheel_sens).powf(-mouse_wheel().1);
+            } else if grabbed || is_mouse_button_down(MouseButton::Left) {
+                let delta = mouse_sens * mouse_delta_position() * Vec2::from(screen_size());
+                phi = (phi + delta.x) % (2.0 * PI);
+                theta = (theta + delta.y).clamp(-0.5 * PI, 0.5 * PI);
+            }
+        }
+
+        {
+            let transorm = Quat::from_euler(EulerRot::ZXY, phi, theta, 0.0);
+            let camera = Camera3D {
+                target: Vec3::ZERO,
+                position: transorm.mul_vec3(Vec3::new(0.0, -r, 0.0)),
+                up: transorm.mul_vec3(Vec3::new(0.0, 0.0, 1.0)),
                 ..Default::default()
             };
             set_camera(&camera);
