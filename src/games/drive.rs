23c7a2e2c95a7df14835a8677537fde96583be5a
Alexey Gerasev <alexey.gerasev@gmail.com>
2024-10-17 18:46:25 +0700

    WIP: Vehicle config

--- src/games/drive.rs
+++ src/games/drive.rs
@@ -4,6 +4,7 @@ use defer::defer;
 use macroquad::{
     camera::{set_camera, set_default_camera, Camera3D},
     color,
+    file::load_file,
     input::{
         is_key_down, is_key_pressed, is_mouse_button_down, mouse_delta_position, mouse_wheel,
         set_cursor_grab, show_mouse, KeyCode, MouseButton,
@@ -14,7 +15,39 @@ use macroquad::{
     texture::{load_texture, set_default_filter_mode, FilterMode},
     window::{clear_background, next_frame},
 };
+use serde::{Deserialize, Deserializer};
 use std::{f32::consts::PI, future::Future, pin::Pin};
+
+fn de_vec3<'de, D: Deserializer<'de>>(deserializer: D) -> Result<Vec3, D::Error> {
+    Ok(Vec3::from(<[f32; 3]>::deserialize(deserializer)?))
+}
+
+#[derive(Clone, Debug, Deserialize)]
+struct WheelConfig {
+    /// Position when no force applied
+    #[serde(deserialize_with = "de_vec3")]
+    position: Vec3,
+    radius: f32,
+    width: f32,
+    /// Spring linear and quadratic hardness (N/m, N/m^2)
+    hardness: (f32, f32),
+    /// Suspension (N/(m/s))
+    suspension: f32,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+struct VehicleConfig {
+    /// Mass in kg
+    mass: f32,
+
+    /// Wheels in order:
+    ///
+    /// + front-left
+    /// + front-right
+    /// + rear-right
+    /// + rear-left
+    wheels: [WheelConfig; 4],
+}
 
 fn grab(state: bool) {
     set_cursor_grab(state);
@@ -27,6 +60,7 @@ pub async fn main() -> Result<(), Error> {
         texture: Some(load_texture("l200.png").await?),
         ..load_model("l200.obj").await?
     };
+    let config = serde_json::from_slice(&load_file("l200.json").await?)?;
 
     let mut grabbed = true;
     grab(grabbed);
